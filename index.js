d663d870e8088d538b3078a62a847ae335c47825
Jasmin Ellis <jasmin.parish@outlook.com>
2020-02-10 05:01:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string)
+{
+ return string.toUpperCase() 
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+
+function shout('I love you, Grandma.')
+{ 
+console.log(string.toUpperCase)
+}
